9259bc6c6a03bb5389e95caa4a8a6d3afd2621ff
BeataMaro <beatamaro@interia.pl>
2023-06-30 23:10:34 +0200

    feat: add assets and variables

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -3,16 +3,21 @@ import './header.scss';
 
 export default function headerComponent() {
   const header = document.createElement('header');
+  const logo = document.createElement('div');
+  logo.className = 'logo';
   const heading1 = document.createElement('h1');
   heading1.textContent = 'Freshwater';
   heading1.classList.add('logo-text');
 
-  // const myIcon = new Image();
-  // myIcon.src = Icon;
-  //   const icon = new URL('./water_desktop.svg', import.meta.url);
-  //   myIcon.src = icon;
+  const logoIcon = new Image();
+  const icon = new URL('../../images/logo-icon.svg', import.meta.url);
+  logoIcon.alt = 'Freshwater logo';
+  logoIcon.className = 'logo-icon';
+  logoIcon.src = icon;
 
-  header.appendChild(heading1);
+  logo.appendChild(logoIcon);
+  logo.appendChild(heading1);
+  header.append(logo);
 
   return header;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -18,32 +19,26 @@ module.exports = {
       template: './src/template.html',
     }),
     new MiniCssExtractPlugin(),
+    new FaviconsWebpackPlugin('./src/images/logo-icon.svg'),
   ],
+  devServer: {
+    static: './dist',
+    port: 5001,
+  },
   module: {
     rules: [
-      //   {
-      //     test: /\.css$/i,
-      //     use: ['style-loader', 'css-loader'],
-      //   },
       {
         test: /\.html$/i,
         loader: 'html-loader',
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /images\/\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
-      //   {
-      //     test: /\.html/,
-      //     type: 'asset/resource',
-      //     generator: {
-      //       filename: 'static/[hash][ext][query]',
-      //     },
-      //   },
     ],
   },
 };
