31ec6614f28154894595d7b03f78aacbb57a1d70
BeataMaro <beatamaro@interia.pl>
2023-07-03 17:49:42 +0200

    feat: implement opening mobile footer menu

--- src/index.js
+++ src/index.js
@@ -6,8 +6,22 @@ const BODY = document.body;
 const hamburgerMenu = document.getElementById('ham-menu');
 hamburgerMenu.addEventListener('click', () => BODY.classList.toggle('open'));
 
-
 // prevent form submitting
 const forms = document.querySelectorAll('form');
 [...forms].map((form) => form.addEventListener('submit', (e) => e.preventDefault()));
+
+// footer zipper
+
+const arrow1 = document.getElementById('arrow-1');
+const arrow2 = document.getElementById('arrow-2');
+
+const menuAccount = document.getElementById('expand-account');
+const menuInfo = document.getElementById('expand-info');
+
+arrow1.addEventListener('click', () => {
+    menuAccount.classList.toggle('desktop')
+});
+arrow2.addEventListener('click', () => {
+    menuInfo.classList.toggle('desktop')
+});
 
