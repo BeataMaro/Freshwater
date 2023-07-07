3decc0fee332bb953374b47a740e0baddc4db872
BeataMaro <beatamaro@interia.pl>
2023-07-07 14:33:17 +0200

    fix: add event delegation for hero nav buttons

--- src/components/heroComponent/hero.js
+++ src/components/heroComponent/hero.js
@@ -1,14 +1,15 @@
 // Hero carousel
 
 const carousel = document.querySelector('.hero-slider');
-const carouselImages = carousel.getElementsByTagName('li');
+const carouselSlides = carousel.getElementsByTagName('li');
 const carouselNavButtons = document.querySelectorAll('.hero-carousel-nav');
+const navButtons = document.querySelector('.hero-slide-controls');
 let currentIndex = 0;
 
 // Show the current slide and hide the rest
 function showSlide(index) {
-  [...carouselImages].map((image) => image.classList.remove('active'));
-  carouselImages[index].classList.add('active');
+  [...carouselSlides].map((slide) => slide.classList.remove('active'));
+  carouselSlides[index].classList.add('active');
 
   [...carouselNavButtons].map((dot) => dot.classList.remove('active'));
   carouselNavButtons[index].classList.add('active');
@@ -16,7 +17,7 @@ function showSlide(index) {
 
 function nextSlide() {
   currentIndex++;
-  if (currentIndex >= carouselImages.length) {
+  if (currentIndex >= carouselSlides.length) {
     currentIndex = 0;
   }
   showSlide(currentIndex);
@@ -25,13 +26,15 @@ function nextSlide() {
 // Automatic slide change
 let slideInterval = setInterval(nextSlide, 3000);
 
-// Event listener for navigation buttons
-[...carouselNavButtons].forEach((button) => {
-  button.addEventListener('click', (e) => {
-    const slideIndex = parseInt(e.target.getAttribute('data-slide-index'));
+// Change slide on click
+navButtons.addEventListener('click', (e) => {
+  let target = e.target;
+
+  if (target.tagName === 'BUTTON') {
+    const slideIndex = parseInt(target.getAttribute('data-slide-index'));
     clearInterval(slideInterval);
     currentIndex = slideIndex;
     showSlide(currentIndex);
     slideInterval = setInterval(nextSlide, 3000);
-  });
+  }
 });
